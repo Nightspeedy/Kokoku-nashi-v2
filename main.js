51436fbbe2e38b86b77acbc5c01111c01642049e
Hampus Lundqvist <hampus0505@gmail.com>
2019-05-21 11:05:17 +0200

    Installed new commadn listener

--- main.js
+++ main.js
@@ -29,36 +29,9 @@ module.exports = class Main {
 
     this.bot.login(this.token)
   }
-
-  handleMessage (message) {
-    // Checking whether or not the code should continue to run
-    if (message.author.bot) return
-    if (message.channel.type === 'dm') return
-
-    // Splitting up command and arguments so they are easily usable and accessible
-    const args = message.content.slice(this.prefix.length).trim().split(/ +/g)
-    const command = args.shift()
-
-    if (message.content.startsWith(this.config.prefix) && !message.content.includes(`${this.config.prefix} `)) this.runCommand(command, message, args)
-  }
-
-  runCommand (command, message, args) {
-    // Trying to run the command
-    console.log(`Shard #${this.bot.shard.id}: Received a command, checking if it's valid`)
-    try {
-      let command = this.bot.cmdhandler.commands.get(command)
-      if (command) console.log(`Shard #${this.bot.shard.id}: Received valid command, running command.`)
-      command.trigger(message, args)
-
-      // And if an error is catched, no such command exists.
-    } catch (error) {
-      console.log(`Shard #${this.bot.shard.id}: Received invalid command, returning.`)
-
-      return message.channel.send('**Error!** Unknown command!')
-    }
-  }
   
   async setupCommands() {
     await this.bot.cmdhandler.install(path.resolve('./commands'), this.DB)
+    this.bot.cmdhandler.listen(this.bot)
   }
 }
