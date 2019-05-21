c846caa236baf36c0b98c94ab98aafd9a695b065
Hampus Lundqvist <hampus0505@gmail.com>
2019-05-21 11:05:54 +0200

    Removed old listener

--- bot.js
+++ bot.js
@@ -6,7 +6,3 @@ const Discord = new Main()
 Discord.bot.on('ready', () => {
   console.log(`Shard #${Discord.bot.shard.id}: logged in!`)
 })
-
-Discord.bot.on('message', message => {
-  Discord.handleMessage(message)
-})
