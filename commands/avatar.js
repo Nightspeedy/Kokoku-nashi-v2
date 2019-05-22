5f2d4796ad8ad8ebbec7c9ac449c21b337a17759
Nightspeedy <b-versteegh@hotmail.nl>
2019-05-22 13:58:39 +0200

    Another small fix in avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -45,13 +45,4 @@ module.exports = class extends Command {
         }
 
     }
-
-    getRandomColor() {
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
 }
