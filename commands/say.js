c6fef825f9751ac82577e25f4e7232b37857e93d
Nightspeedy <b-versteegh@hotmail.nl>
2019-05-23 13:07:42 +0200

    k!say command

---
+++ commands/say.js
@@ -0,0 +1,46 @@
+
+const Command = require('@lib/command')
+const { RichEmbed } = require('discord.js')
+
+module.exports = class extends Command {
+    constructor (bot) {
+        super({
+            name: 'say',
+            description: "Make the bot say something",
+            type: "social",
+            args: "{#channel} {'message in quotations'}"
+        }) // Pass the appropriate command information to the base class.
+        this.fetch.member = false // Fetch the Member object from DB on trigger.
+
+        this.bot = bot
+    }
+
+    async run ({ message, args }) {
+
+        message.delete();
+
+        let string = args[1];
+
+        if (!string) (
+
+            string = args[0]
+
+        )
+
+        if(!message.mentions.channels.first()){
+            
+            message.channel.send(string);
+
+        } else {
+            
+            try {
+
+                message.guild.channels.get(message.mentions.channels.first().id).send(string)
+            } catch(err) {
+
+                message.channel.send("**ERROR!** You can only send messages to a channel in this server!")
+                
+            }
+        }
+    }
+}
