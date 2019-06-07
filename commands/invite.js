a43a698730bd2b8f3dd0368b3ed95d54c66f6b62
Nightspeedy <b-versteegh@hotmail.nl>
2019-06-07 09:42:24 +0200

    last fix in invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -10,7 +10,6 @@ module.exports = class extends Command {
       args: '',
       permissions: [PERMISSIONS.GENERAL]
     }) // Pass the appropriate command information to the base class.
-    this.fetch.member = true // Fetch the Member object from DB on trigger.
 
     this.bot = bot
   }
