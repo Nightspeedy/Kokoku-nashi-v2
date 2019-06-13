9ddc75308914d0f8388e5f5cb9127929a889a5a9
Nightspeedy <b-versteegh@hotmail.nl>
2019-06-13 11:10:17 +0200

    k!profile now sends a proper embed

--- commands/profile.js
+++ commands/profile.js
@@ -2,6 +2,7 @@ const Command = require('@lib/command')
 const TYPES = require('@lib/types')
 const ERROR = require('@lib/errors')
 const PERMISSIONS = require('@lib/permissions')
+const { RichEmbed } = require('discord.js')
 
 module.exports = class extends Command {
   constructor (bot) {
@@ -19,9 +20,41 @@ module.exports = class extends Command {
     this.bot = bot
   }
 
-  async run ({ message, args, member }) {
+  async run ({ message, args, member, color }) {
 
-    return message.reply(JSON.stringify(member))
+    if (!args[0]) {
+ 
+      let nxtLvl = member.level * 200;
 
+      const embed = new RichEmbed()
+      .setTitle(message.author.username + "'s Profile")
+      .setThumbnail(message.author.displayAvatarURL)
+      .setColor(color)
+      .addField("Level", member.level)
+      .addField("Next level progress", member.exp + "/" + nxtLvl + " Exp")
+      .addField("Reputation", member.reputation)
+      .addField("Credits", member.credits);
+
+      message.channel.send(embed);
+
+  } else if (args[0]) {
+
+      if (!member) return message.channel.send(this.error(ERROR.MEMBER_NOT_FOUND, { message, args }))
+      //if (message.mentions.members.first().user.bot) return message.channel.send("**Error!** Target user is a bot!");
+
+      let nxtLvl = member.level * botconfig.level;
+
+      const embed = new RichEmbed()
+      .setTitle(message.mentions.members.first().user.username + "'s Profile")
+      .setThumbnail(message.mentions.members.first().user.displayAvatarURL)
+      .setColor(color)
+      .addField("Level", member.level)
+      .addField("Next level progress", member.Values.exp + "/" + nxtLvl + " Exp")
+      .addField("Reputation", member.reputation)
+      .addField("Credits", member.credits);
+
+      message.channel.send(embed);
+
+    }
   }
 }
