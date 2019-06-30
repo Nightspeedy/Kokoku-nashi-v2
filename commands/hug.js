692361744f5c8860c861c146bdfaaae0a0961be8
Nightspeedy <b-versteegh@hotmail.nl>
2019-06-30 22:54:18 +0200

    k!hug command

---
+++ commands/hug.js
@@ -0,0 +1,39 @@
+const Command = require('@lib/command')
+const TYPES = require('@lib/types')
+const ERROR = require('@lib/errors')
+const PERMISSIONS = require('@lib/permissions')
+const { RichEmbed } = require('discord.js')
+const gifs = require('@lib/socialGifs.json')
+
+module.exports = class extends Command {
+  constructor (bot) {
+    super({
+      name: 'hug',
+      aliases: ['huggu'],
+      description: 'Gives your best buddy a hug! This includes yourself.',
+      type: TYPES.SOCIAL,
+      args: '[@mention]',
+    }) // Pass the appropriate command information to the base class.
+
+    this.bot = bot
+  }
+
+  async run ({ message, args, color }) {
+    let gif = Math.floor(Math.random() * gifs.hug.length);
+
+    let embed = new RichEmbed()
+    .setColor(color)
+    .setDescription(`image not loading? click [here](${gifs.hug[gif]})`);
+
+    if (!args[0]) {
+        embed.setTitle("Ahw... have a hug :3")
+        await embed.setImage(gifs.hug[gif])
+        return message.channel.send(embed).catch(err => {if(err) console.log(err)});
+    } else if (args[0]) {
+        if (args[0] != message.mentions.members.first()) return this.error(ERROR.MEMBER_NOT_FOUND, {message,args})
+        embed.setTitle(`${message.author.username} hugged ${message.mentions.members.first().user.username}!`)
+        await embed.setImage(gifs.hug[gif])
+        return message.channel.send(embed).catch(err => {if(err) console.log(err)});
+    }
+  }
+}
