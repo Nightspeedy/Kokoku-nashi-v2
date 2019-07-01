5316804fa9f6bc06b36b654b740b861173c4563c
Nightspeedy <b-versteegh@hotmail.nl>
2019-07-01 12:35:44 +0200

    Guild delete event

---
+++ events/guildDelete.js
@@ -0,0 +1,14 @@
+const Event = require('@lib/event')
+const { Guild } = require('@lib/models')
+
+module.exports = class extends Event {
+  constructor (main) {
+    super({ event: 'guildDelete' })
+    this.bot = main.bot
+  }
+
+  async trigger (guild) {
+    let guildToAdd = await Guild.findOne({ id: guild.id })
+    if (guildToAdd) Guild.remove({ id: guild.id })
+  }
+}
