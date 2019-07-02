7fc7cd2bf0ae076013b544ae29872125021a8a85
Nightspeedy <b-versteegh@hotmail.nl>
2019-07-02 19:45:44 +0200

    Added k!rep

---
+++ commands/rep.js
@@ -0,0 +1,69 @@
+const Command = require('@lib/command')
+const TYPES = require('@lib/types')
+const ERROR = require('@lib/errors')
+const PERMISSIONS = require('@lib/permissions')
+const { RichEmbed } = require('discord.js')
+const { Member } = require('@lib/models')
+
+module.exports = class extends Command {
+  constructor (bot) {
+    super({
+      name: 'reputation',
+      aliases: ['rep', 'giverep'],
+      description: 'Send the bot invite URL via DM',
+      type: TYPES.SOCIAL,
+      args: '',
+    }) // Pass the appropriate command information to the base class.
+
+    this.bot = bot
+  }
+
+  async run ({ message, color, args }) {
+
+    // Gift by mention
+    if (!message.mentions.members.first()) return this.error(ERROR.MEMBER_NOT_FOUND, {message,args})
+    if (message.mentions.members.first().user.bot) return this.error({message: 'Bots dont have profiles!'}, {message, args})//message.channel.send("**Error!** Target user is a bot!");
+    if (message.author.id == message.mentions.members.first().user.id) return this.error({message: 'You cannot give yourself reputation!'}, {message, args})//message.channel.send("**Error!** You cannot rep yourself!");
+
+    let member = await Member.findOne({id: message.author.id})
+    let memberMention = await Member.findOne({id: message.mentions.members.first().id})
+
+    if (!memberMention) return this.error(ERROR.UNKNOWN_MEMBER, {message, args})
+
+    if (member.repLastGiven + 86100000 > Date.now()) {
+
+        let hours, minutes, seconds
+
+        let totalSeconds = (member.repLastGiven + 86100000) - Date.now()
+
+        totalSeconds = totalSeconds / 1000
+
+        hours = Math.floor(totalSeconds / 3600)
+        totalSeconds %= 3600
+        minutes = Math.floor(totalSeconds / 60)
+        seconds = Math.floor(totalSeconds % 60)
+
+        return this.error({message: `Cooldown active. You can use this command again in: ${hours} hours, ${minutes} minutes, and ${seconds} seconds.`}, {message,args})
+    } else {
+      try {
+
+        let reputation = memberMention.reputation
+
+        reputation += 1;
+        console.log(member.repLastGiven)
+        await member.updateOne({repLastGiven: Date.now()})
+        await memberMention.updateOne({reputation: reputation});
+
+        const embed = new RichEmbed()
+        .setTitle(message.author.username)
+        .setColor(color)
+        .addField("You repped someone! ", message.author.username + " added reputation to " + message.mentions.members.first().user.username)
+        message.channel.send(embed).catch(err => {});
+
+      } catch(e) {
+        console.log(e)
+        return this.error(ERROR.TRY_AGAIN, {message, args})
+      }
+    }
+  }
+}
