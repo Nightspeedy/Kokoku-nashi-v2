29ea216586b1ac8e2ff926bc2007fc2f237797bc
Nightspeedy <b-versteegh@hotmail.nl>
2019-07-04 12:34:46 +0200

    new command

---
+++ commands/membercount.js
@@ -0,0 +1,40 @@
+const Command = require('@lib/command')
+const TYPES = require('@lib/types')
+const ERROR = require('@lib/errors')
+const PERMISSIONS = require('@lib/permissions')
+const { RichEmbed } = require('discord.js')
+const { Member } = require('@lib/models')
+
+module.exports = class extends Command {
+  constructor (bot) {
+    super({
+      name: 'membercount',
+      aliases: ['serversize'],
+      description: 'Shows your server\'s membercount!',
+      type: TYPES.UTILITY,
+      args: 'This command has no arguments',
+    }) // Pass the appropriate command information to the base class.
+
+    this.fetch.member = true
+
+    this.bot = bot
+  }
+
+  async run ({ message, color }) {
+
+    let guild = await message.guild.fetchMembers()
+    let totalUsers = guild.members.size
+    let memberCount = guild.members.filter(member => !member.user.bot).size
+
+    let botCount = totalUsers - memberCount
+
+    let embed = new RichEmbed()
+    .setTitle("Guild membercount")
+    .setColor(color)
+    .setDescription(`Total members in guild: ${totalUsers} \n\n${botCount} Bot's\n${memberCount} Humans`)
+    message.channel.send(embed)
+
+  }
+}
+
+// msg.guild.members.filter(member => !member.user.bot).size
