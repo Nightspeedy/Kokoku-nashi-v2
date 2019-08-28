265c3b5334caba733357ab8d38094cb415262e89
Nightspeedy <b-versteegh@hotmail.nl>
2019-08-28 17:32:18 +0200

    Updated roadmap command to use the new strings database

--- commands/roadmap.js
+++ commands/roadmap.js
@@ -3,6 +3,8 @@ const TYPES = require('@lib/types')
 const ERROR = require('@lib/errors')
 const PERMISSIONS = require('@lib/permissions')
 const { RichEmbed } = require('discord.js')
+const { Strings } = require('@lib/models')
+
 
 module.exports = class extends Command {
   constructor (bot) {
@@ -13,9 +15,6 @@ module.exports = class extends Command {
       args: '',
     }) // Pass the appropriate command information to the base class.
 
-    this.wantToAdd = ['Fun commands', 'Event loggers, (send a logfile if something happens)', 'Music', 'Warning system', 'Punishment based on warnings', 'Muting system']
-    this.workingOn = ['Moderation', "An interactive configuration command"]
-    this.completed = ['Autoroles', 'Welcome/Leave messages', 'Global Profiles', 'Global levels', 'Profile titles/decriptions', 'Double global EXP/Coins for premium servers']
     this.bot = bot
   }
 
@@ -25,29 +24,30 @@ module.exports = class extends Command {
     .setTitle('Roadmap')
     .setColor(color)
 
-    let wantToAdd = ''
-    let workingOn = ''
-    let completed = ''
+    let wantToAdd = await Strings.findOne({key: 'wantToAdd'})// ['Fun commands', 'Event loggers, (send a logfile if something happens)', 'Music', 'Warning system', 'Punishment based on warnings', 'Muting system']
+    let workingOn = await Strings.findOne({key: 'workingOn'})// ['Moderation', "An interactive configuration command"]
+    let completed = await Strings.findOne({key: 'completed'})// ['Autoroles', 'Welcome/Leave messages', 'Global Profiles', 'Global levels', 'Profile titles/decriptions', 'Double global EXP/Coins for premium servers']
+    
 
-    for (let i = 0; i < this.wantToAdd.length; i++) {
+    // for (let i = 0; i < this.wantToAdd.length; i++) {
 
-        wantToAdd += "- " + this.wantToAdd[i] + "\n"
+    //     wantToAdd += "- " + this.wantToAdd[i] + "\n"
 
-    }
-    for (let i = 0; i < this.workingOn.length; i++) {
+    // }
+    // for (let i = 0; i < this.workingOn.length; i++) {
 
-        workingOn += "- " + this.workingOn[i] + "\n"
+    //     workingOn += "- " + this.workingOn[i] + "\n"
 
-    }
-    for (let i = 0; i < this.completed.length; i++) {
+    // }
+    // for (let i = 0; i < this.completed.length; i++) {
 
-        completed += "- " + this.completed[i] + "\n"
+    //     completed += "- " + this.completed[i] + "\n"
 
-    }
+    // }
 
-    embed.addField("Features we want to add (updated as we think of more)", wantToAdd)
-    .addField("Features we're working on", workingOn)
-    .addField("Features we've completed", completed)
+    embed.addField("Features we want to add (updated as we think of more)", wantToAdd.value)
+    .addField("Features we're working on", workingOn.value)
+    .addField("Features we've recently completed", completed.value)
 
     message.channel.send(embed).catch(e => {})
 
