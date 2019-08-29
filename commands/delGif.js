dbe34698abaff2d21aee0a9227722a2c4918da9a
memeone <hampus0505@gmail.com>
2019-08-29 18:11:33 +0200

    linted

--- commands/delGif.js
+++ commands/delGif.js
@@ -5,47 +5,46 @@ const { RichEmbed } = require('discord.js')
 const { Gifs } = require('@lib/models')
 
 module.exports = class extends Command {
-    constructor (bot) {
-        super({
-            name: 'deletegif',
-            description: 'Add a gif image to the database',
-            type: TYPES.BOT_OWNER,
-            args: '{type} {image url}'
-        }) // Pass the appropriate command information to the base class.
+  constructor (bot) {
+    super({
+      name: 'deletegif',
+      description: 'Add a gif image to the database',
+      type: TYPES.BOT_OWNER,
+      args: '{type} {image url}'
+    }) // Pass the appropriate command information to the base class.
 
-        this.fetch.guild = true
+    this.fetch.guild = true
 
-        this.bot = bot
+    this.bot = bot
+  }
+
+  async run ({ message, args, guild, color }) {
+    if (!args[0] || !args[1]) return this.error(ERROR.INVALID_ARGUMENTS, { message, args })
+
+    if (!args[1].endsWith('.gif')) return this.error({ message: "Can't remove image from the DB, image is not a valid GIF" })
+
+    switch (args[0]) {
+      case 'hug':
+        this.removeGif('hug', args[1], message)
+        break
+      case 'kiss':
+        this.removeGif('kiss', args[1], message)
+        break
+      case 'poke':
+        this.removeGif('poke', args[1], message)
+        break
+      case 'slap':
+        this.removeGif('slap', args[1], message)
+        break
+      default:
+        return this.error(ERROR.INVALID_ARGUMENTS, { message, args })
     }
+  }
 
-    async run ({ message, args, guild, color }) {
-
-        if (!args[0] || !args[1]) return this.error(ERROR.INVALID_ARGUMENTS, {message, args})
-
-        if (!args[1].endsWith('.gif')) return this.error({message: "Can't remove image from the DB, image is not a valid GIF"})
-
-        switch(args[0]) {
-            case 'hug':
-                this.removeGif("hug", args[1], message)
-                break;
-            case 'kiss':
-                this.removeGif("kiss", args[1], message)
-                break;
-            case 'poke':
-                this.removeGif("poke", args[1], message)
-                break;
-            case 'slap':
-                this.removeGif("slap", args[1], message)
-                break;
-            default:
-                return this.error(ERROR.INVALID_ARGUMENTS, {message, args})
-        }
-
-    }
-    async removeGif(type, gifUrl, message) {
-        let gif = await Gifs.findOne({gifType: type, url: gifUrl})
-        if (!gif) return this.error({message: "This gif is not in the DB!"}, {message})
-        await Gifs.deleteOne({gifType: type, url: gifUrl})
-        message.channel.send("Successfully removed gif!")
-    }
+  async removeGif (type, gifUrl, message) {
+    const gif = await Gifs.findOne({ gifType: type, url: gifUrl })
+    if (!gif) return this.error({ message: 'This gif is not in the DB!' }, { message })
+    await Gifs.deleteOne({ gifType: type, url: gifUrl })
+    message.channel.send('Successfully removed gif!')
+  }
 }
--- commands/setString.js
+++ commands/setString.js
@@ -5,41 +5,40 @@ const { RichEmbed } = require('discord.js')
 const { Strings } = require('@lib/models')
 
 module.exports = class extends Command {
-    constructor (bot) {
-        super({
-            name: 'setstring',
-            description: 'Change a string value',
-            type: TYPES.BOT_OWNER,
-            args: '{key} {value}'
-        }) // Pass the appropriate command information to the base class.
+  constructor (bot) {
+    super({
+      name: 'setstring',
+      description: 'Change a string value',
+      type: TYPES.BOT_OWNER,
+      args: '{key} {value}'
+    }) // Pass the appropriate command information to the base class.
 
-        this.fetch.guild = true
+    this.fetch.guild = true
 
-        this.bot = bot
+    this.bot = bot
+  }
+
+  async run ({ message, args, guild, color }) {
+    if (!args[0] || !args[1]) return this.error(ERROR.INVALID_ARGUMENTS, { message, args })
+
+    let keyValuePair = await Strings.findOne({ key: args[0] })
+    const keys = await Strings.find({ })
+    console.log(keys)
+
+    if (!keyValuePair) {
+      console.log('Created')
+      await Strings.create({ key: args[0], value: args[1] })
+    } else {
+      try {
+        console.log('Updated')
+        await keyValuePair.update({ value: args[1] })
+        keyValuePair = await Strings.findOne({ key: args[0] })
+        console.log(keyValuePair)
+        this.success('Success!', 'Updated KeyValue pair successfully!', { message })
+      } catch (e) {
+        console.log(e)
+        if (e) return this.error(ERROR.UNKNOWN_ERROR, { message, args })
+      }
     }
-
-    async run ({ message, args, guild, color }) {
-    
-        if (!args[0] || !args[1]) return this.error(ERROR.INVALID_ARGUMENTS, {message, args})
-
-        let keyValuePair = await Strings.findOne({key: args[0]})
-        let keys = await Strings.find({ })
-        console.log(keys)
-
-        if (!keyValuePair) {
-            console.log("Created")
-            await Strings.create({key: args[0], value: args[1]})
-        } else {
-            try {
-                console.log("Updated")
-                await keyValuePair.update({value: args[1]})
-                keyValuePair = await Strings.findOne({key: args[0]})
-                console.log(keyValuePair)
-                this.success("Success!","Updated KeyValue pair successfully!", {message})
-            } catch(e) {
-                console.log(e)
-                if (e) return this.error(ERROR.UNKNOWN_ERROR, {message, args})
-            }
-        }
-    }
+  }
 }
