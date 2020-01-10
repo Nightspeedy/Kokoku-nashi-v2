c19e6a0368a645f7965cfdcb8bbfb04959b713a9
toman222 <tobi.berger13@gmail.com>
2020-01-10 12:15:18 +0100

    !Waiting on bot to get timedActionHandler!

---
+++ commands/reminder.js
@@ -0,0 +1,36 @@
+const Command = require('@lib/command')
+const TYPES = require('@lib/types')
+const ERROR = require('@lib/errors')
+
+module.exports = class extends Command {
+  constructor (bot) {
+    super({
+      name: 'reminder',
+      aliases: ['reminder', 'rem'],
+      description: 'Reminds you of something in several minutes.',
+      type: TYPES.UTILITY,
+      args: '{time in minutes} {...message to send}'
+    }) // Pass the appropriate command information to the base class.
+
+    this.bot = bot
+  }
+
+  async run ({ message, args, guild }) {
+    const minutes = parseInt(args[0])
+    const content = args.slice(1).join(' ')
+
+    if (!minutes || !content) {
+      return this.error(ERROR.INVALID_ARGUMENTS, { message })
+    }
+
+    this.bot.timedActionHandler.push('sendMessage', minutes * 1000, {
+      channel: message.channel.id,
+      content: {
+        embed: {
+          title: `${minutes} minutes ago you asked to be reminded of:`,
+          description: content
+        }
+      }
+    })
+  }
+}
