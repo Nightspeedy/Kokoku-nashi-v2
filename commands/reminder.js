1bbf5c7eedbf83d4f374f48f49dfa5be248a38a7
toman222 <tobi.berger13@gmail.com>
2020-01-11 12:10:18 +0100

    *Pluralization +actual feedback +more argument checks

--- commands/reminder.js
+++ commands/reminder.js
@@ -19,18 +19,25 @@ module.exports = class extends Command {
     const minutes = parseInt(args[0])
     const content = args.slice(1).join(' ')
 
-    if (!minutes || !content) {
+    if (!minutes || minutes <= 0 || !content) {
       return this.error(ERROR.INVALID_ARGUMENTS, { message })
     }
 
-    await this.bot.timedActionHandler.push('sendDM', minutes * 60 * 1000, {
+    const timedAction = await this.bot.timedActionHandler.push('sendDM', minutes * 60 * 1000, {
       userID: message.author.id,
       content: {
         embed: {
-          title: `${minutes} minutes ago you asked to be reminded of:`,
+          title: `${minutes} minute${minutes !== 1 ? 's' : ''} ago you asked to be reminded of:`,
           description: content
         }
       }
     })
+
+    if (!timedAction) {
+      console.error(timedAction)
+      this.error(ERROR.UNKNOWN_ERROR, { message, args })
+    } else {
+      message.channel.send(`Successfully set a reminder for ${minutes} minute${minutes !== 1 ? 's' : ''} from now.`)
+    }
   }
 }
